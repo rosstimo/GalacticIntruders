466bfb1db51b7ad75c90fb70768993aeffee2260
rosstimo <rosstimo@isu.edu>
2022-04-07 08:59:21 -0600

    add read key test

--- GalacticIntrudersGame/GalacticIntrudersGame/GalacticIntrudersGame.vb
+++ GalacticIntrudersGame/GalacticIntrudersGame/GalacticIntrudersGame.vb
@@ -1,7 +1,20 @@
 ﻿Module GalacticIntrudersGame
 
     Sub Main()
+        ReadKeyTest()
+        'Console.Read()
+    End Sub
 
+    ''' <summary>
+    ''' see https://docs.microsoft.com/en-us/dotnet/api/system.console.readkey?view=netframework-4.7.2
+    ''' </summary>
+    Sub ReadKeyTest()
+        Dim key As String
+
+        Do
+            key = Console.ReadKey(True).KeyChar
+            MsgBox(key)
+        Loop While key <> "q"
     End Sub
 
 End Module
