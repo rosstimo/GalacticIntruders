672c63c84da1dab1a061e09549c8f831fc739567
Miranda Breves <brevmira@isu.edu>
2022-04-13 17:18:23 -0600

    Updated the Main sub to try to merge with the original fork

--- GalacticIntrudersGame/GalacticIntrudersGame/GalacticIntrudersGame.vb
+++ GalacticIntrudersGame/GalacticIntrudersGame/GalacticIntrudersGame.vb
@@ -5,6 +5,21 @@
 
         animation.BeginAnimationConsole()
 
+        ReadKeyTest()
+        'Console.Read()
+
+    End Sub
+
+    ''' <summary>
+    ''' see https://docs.microsoft.com/en-us/dotnet/api/system.console.readkey?view=netframework-4.7.2
+    ''' </summary>
+    Sub ReadKeyTest()
+        Dim key As String
+
+        Do
+            key = Console.ReadKey(True).KeyChar
+            MsgBox(key)
+        Loop While key <> "q"
     End Sub
 
 End Module
